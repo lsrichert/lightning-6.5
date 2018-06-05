a03c6da1529125302bb28a2a3df0e238bb100d62
Lauren Richert <lsrichert@gmail.com>
2018-06-05 12:08:10 -0500

    challenge

---
+++ main.js
@@ -0,0 +1,21 @@
+// 1. Write a function that takes two arguments. This function should return the concatenated value of the two arguments.
+
+// 2. Call the function with your first name and last name as the two arguments.
+
+// 3. Console log the return value.
+
+// this is one way to do this challenge
+
+// ```const concatenate = function(firstArgument, secondArgument) {
+//     // return `${firstArgument} ${secondArgument}`
+//   }
+  
+//   console.log(concatenate("Jisie", "David"))```
+
+//   ____________________________________________
+
+const fullName = function(firstName, lastName) {
+    return `${firstName} ${lastName}`
+}
+
+console.log(fullName("Lauren", "Richert"))
